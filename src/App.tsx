9d3c0717e0d5263b284b21ac26a9c8515509c46d
Long Tran <57053685+baolongt@users.noreply.github.com>
2023-05-30 21:42:28 +0700

    add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,46 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Editor from './components/ckedtior/WrappedEditor';
-// import Counter from './components/counter/CounterComponent';
+import Dashboard from './components/layout/Dashboard';
+import { Box } from '@mui/material';
+import useCurrentPath from './hooks/useCurrentPath';
+
+const Placeholder: React.FC = () => {
+  const path = useCurrentPath();
+  return (
+    <Box
+      height="500px"
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      textAlign="center"
+    >
+      <span>This is path {path}</span>
+    </Box>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <Editor />
-      {/* <Counter /> */}
-    </div>
+    <Dashboard>
+      <Routes>
+        <Route path="/">
+          <Route index={true} element={<Placeholder />}></Route>
+          <Route index={false} path="editor" element={<Editor />}></Route>
+          <Route index={false} path="Index" element={<Placeholder />}></Route>
+          <Route index={false} path="Started" element={<Placeholder />}></Route>
+          <Route index={false} path="route">
+            <Route index={true} element={<>test</>}></Route>
+            <Route
+              index={false}
+              path="editor"
+              element={<h1>editor</h1>}
+            ></Route>
+          </Route>
+        </Route>
+      </Routes>
+    </Dashboard>
   );
 }
 
