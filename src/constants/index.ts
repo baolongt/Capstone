60e51c41ee7ef72b87a94cb1be07022afb6f3da9
Long Tran <57053685+baolongt@users.noreply.github.com>
2023-06-25 21:23:02 +0700

    adđ consstancts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,1 @@
-export const API_URL = 'https://dmscapstonebe.azurewebsites.net'
+export * as api from './api';
--- src/constants/routes.tsx
+++ src/constants/routes.tsx
@@ -1,16 +1,13 @@
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import FolderIcon from '@mui/icons-material/Folder';
-// import SettingsIcon from '@mui/icons-material/Settings';
-// import PersonIcon from '@mui/icons-material/Person';
-// import { ReactNode } from 'react';
 import {
   AdminPanelSettings,
   SettingsApplicationsSharp
 } from '@mui/icons-material';
 import React from 'react';
-import { Path } from '../models/path';
-// import { dark } from '@mui/material/styles/createPalette';
+import { Path } from '../types';
 import Diversity3Icon from '@mui/icons-material/Diversity3';
+
 const incomingDocumentSubPaths: Path[] = [
   {
     label: 'Sổ văn bản đến',
