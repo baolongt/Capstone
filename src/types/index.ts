d2f40bbfe6a18d3fcaedba65016e36f8958f61e7
Long Tran <57053685+baolongt@users.noreply.github.com>
2023-07-29 10:53:16 +0700

    remove isolatedModules

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,5 @@
-export { type Column } from './column';
-export { type Path } from './path.types';
-export { type SelectOption } from './selectItem.types';
-export { type TableState } from './table';
+export { Column } from './column';
+export { Path } from './path.types';
+export { SelectOption } from './selectItem.types';
+export { TableState } from './table';
 export type Nullable<T> = T | null;
