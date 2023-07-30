d21950a651ede25396f51187df2616ee2b068629
Long Tran <57053685+baolongt@users.noreply.github.com>
2023-07-30 14:52:22 +0700

    add new model

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,4 +3,6 @@ export * as department from './department';
 export * as document from './document';
 export * as outgoingDocument from './outgoingDocument';
 export * as role from './role';
+export * as file from './uploadFile';
+export { UploadFile } from './uploadFile';
 export * as user from './user';
