332ffbd301582e52b82b53894a253c9534c229d0
Long Tran <57053685+baolongt@users.noreply.github.com>
2023-08-06 08:50:08 +0700

    Rename model

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,6 @@
 export * as common from './common';
 export * as department from './department';
-export * as document from './document';
+export * as file from './file';
 export * as outgoingDocument from './outgoingDocument';
 export * as role from './role';
 export * as user from './user';
