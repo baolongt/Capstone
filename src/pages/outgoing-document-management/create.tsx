afffd81cf455bd4e4ab064c40125285c4f6a770d
Long Tran <57053685+baolongt@users.noreply.github.com>
2023-08-16 15:59:43 +0700

    Refactor

--- src/pages/outgoing-document-management/create.tsx
+++ src/pages/outgoing-document-management/create.tsx
@@ -1,10 +1,11 @@
 import { yupResolver } from '@hookform/resolvers/yup';
+import { Box, Grid, Typography } from '@mui/material';
 import React, { useEffect } from 'react';
 import { useForm, UseFormReturn } from 'react-hook-form';
 import { toast } from 'react-toastify';
 
 import { useUploadForm } from '@/apis/outgoingDocument';
-import CreateDocumentForm from '@/components/document/outgoing/CreateDocumentForm';
+import CreateDocumentForm from '@/components/document/outgoing/outgoing-doc-create-form';
 import { UploadFile } from '@/models';
 import { validation } from '@/types';
 
@@ -50,13 +51,20 @@ const CreateOutgoingDocumentPage: React.FC = () => {
   }, [isSuccess, reset]);
 
   return (
-    <>
-      <CreateDocumentForm
-        isSubmitForm={isLoading}
-        handleSubmitForm={handleSubmitForm}
-        form={form}
-      />
-    </>
+    <Box sx={{ px: 5 }}>
+      <Grid container spacing={2} sx={{ mb: 3, mt: 3 }}>
+        <Grid item xs={12} sx={{ mx: 10 }}>
+          <Typography variant="h5">Đăng ký văn bản đi</Typography>
+        </Grid>
+        <Grid item xs={12} sx={{ mt: 2, mx: 10 }}>
+          <CreateDocumentForm
+            isSubmitForm={isLoading}
+            handleSubmitForm={handleSubmitForm}
+            form={form}
+          />
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
