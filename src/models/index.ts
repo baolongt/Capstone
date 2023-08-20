7b0fbf39660d5a5b3735c2f5cb610fd4612d3ae8
Long Tran <57053685+baolongt@users.noreply.github.com>
2023-08-20 16:42:12 +0700

    update route

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,4 @@
+export * as auth from './auth';
 export * as common from './common';
 export * as department from './department';
 export * as file from './file';
@@ -5,3 +6,4 @@ export * as outgoingDocument from './outgoingDocument';
 export * as role from './role';
 export * from './uploadFile';
 export * as user from './user';
+export * as validation from './validation';
