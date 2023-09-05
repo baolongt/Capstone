4956fad245b855d219022ddd08da4774d4ef33a1
Long Tran <57053685+baolongt@users.noreply.github.com>
2023-09-05 13:54:54 +0700

    update login

--- src/models/auth.ts
+++ src/models/auth.ts
@@ -2,3 +2,31 @@ export type LoginPayload = {
   email: string;
   password: string;
 };
+
+export type Auth = {
+  id: number;
+  name: string;
+  password: string;
+  email: string;
+  citizenIdentification: string;
+  roleID: number;
+  jobPositionID: number;
+  departmentId: number;
+  role: {
+    id: number;
+    name: string;
+  };
+  jobPosition: null;
+  department: {
+    id: number;
+    name: string;
+    departmentLeaderId: number;
+    departmentLeader: null;
+    members: null[];
+  };
+  createdDate: string;
+  createdById: number;
+  updatedDate: string;
+  updatedBy: number;
+  isDeleted: boolean;
+};
