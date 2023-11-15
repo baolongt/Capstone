d641e93f3cbb2816299e299d51b39f4a70f19cba
Long Tran <57053685+baolongt@users.noreply.github.com>
2023-11-15 18:44:36 +0700

    updat env

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,28 @@
 import react from '@vitejs/plugin-react';
 import { fileURLToPath } from 'url';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      // for TypeScript path alias import like : @/x/y/z
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv('mock', process.cwd(), '');
+  const processEnvValues = {
+    'process.env': Object.entries(env).reduce((prev, [key, val]) => {
+      console.log(key, val);
+      return {
+        ...prev,
+        [key]: val
+      };
+    }, {})
+  };
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        // for TypeScript path alias import like : @/x/y/z
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    define: processEnvValues
+  };
 });
