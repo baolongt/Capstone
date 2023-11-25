17532411b476b3e1c5526065d0692aa8d63c18bd
Long Tran <57053685+baolongt@users.noreply.github.com>
2023-11-25 23:32:43 +0700

    update

--- src/apis/signalR/index.ts
+++ src/apis/signalR/index.ts
@@ -13,6 +13,7 @@ export async function send(id: string) {
     await setUserName('test');
     await send(id as string);
   } catch (e) {
+    console.log('sign error', e);
     toast.error('Không thể kết nối đến thiết bị ký số');
   }
 }
