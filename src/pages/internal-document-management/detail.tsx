1d74c0f6ad8f28fcf160199e2e92c67d812b6e30
Long Tran <57053685+baolongt@users.noreply.github.com>
2023-12-02 11:27:43 +0700

    Add deadline for step (#97)
    * update to localhost:5001
    
    * add noti
    
    * update
    
    * update
    
    * update
    
    * update
    
    * add by pass file
    
    * update index.html
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update log
    
    * update
    
    * add deadline for step

--- src/pages/internal-document-management/detail.tsx
+++ src/pages/internal-document-management/detail.tsx
@@ -1,36 +1,23 @@
-import PostAddIcon from '@mui/icons-material/PostAdd';
-import {
-  Box,
-  Divider,
-  IconButton,
-  Paper,
-  Stack,
-  Tooltip,
-  Typography,
-  useTheme
-} from '@mui/material';
+import { Box, Paper, Stack } from '@mui/material';
 import * as React from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
 import { send } from '@/apis';
 import { DocTypeEnum } from '@/apis/file/addDocToFile';
 import { useGetOneDocument } from '@/apis/internalDocument/getOneDocument';
-import { Loading } from '@/components/common';
+import { CustomButton, Loading } from '@/components/common';
 import AppDocViewer from '@/components/common/document-viewer';
 import PageHeader from '@/components/common/page-header';
 import PageTitle from '@/components/common/page-title';
 import { AddDocToFileDialog } from '@/components/dialogs';
 import { DetailAttachmentAccordion } from '@/components/document';
-import DocComment from '@/components/document/comment';
 import {
   DetailDescription,
   DetailTimeline
 } from '@/components/document/internal';
 import { Attachment } from '@/models';
-import { DocumentType } from '@/models/comment';
 
 const InternalDocumentDetail = () => {
-  const theme = useTheme();
   const { id } = useParams<{ id: string }>();
   const { data, isLoading } = useGetOneDocument(id ? parseInt(id) : -1);
   const [docPreview, setDocPreview] = React.useState(false);
@@ -39,13 +26,6 @@ const InternalDocumentDetail = () => {
   );
   const [openAddDocToFile, setOpenAddDocToFile] = React.useState(false);
   const navigate = useNavigate();
-  React.useEffect(() => {
-    if (data) {
-      if (data.registrationStatus === 1) {
-        navigate('/internal-documents/create?step=2&&id=' + data.id);
-      }
-    }
-  }, [data]);
 
   if (isLoading) {
     return <Loading />;
@@ -93,11 +73,10 @@ const InternalDocumentDetail = () => {
               mt: 1
             }}
           >
-            <Tooltip title="Thêm vào sổ công việc">
-              <IconButton color="info" onClick={handleOpenAddDocToFile}>
-                <PostAddIcon />
-              </IconButton>
-            </Tooltip>
+            <CustomButton
+              label="Thêm vào sổ công việc"
+              onClick={handleOpenAddDocToFile}
+            />
           </Stack>
         </PageHeader>
         <Box
@@ -105,44 +84,14 @@ const InternalDocumentDetail = () => {
           component={Paper}
         >
           <DetailDescription data={data} sx={{ width: '100%' }} />
-          <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-            <DetailTimeline
-              sx={{
-                p: 2,
-                mt: 2,
-                overflow: 'auto',
-                maxHeight: '40vh',
-                width: '45%',
-                py: 3
-              }}
-              processHistory={data.processHistory}
-            />
-            <DetailAttachmentAccordion
-              attachments={data.attachments as Attachment[]}
-              watchAttachment={watchAttachment}
-              signAttachment={signAttachment}
-              addNumber={handleAddNumber}
-              sx={{
-                p: 2,
-                mt: 2,
-                overflow: 'auto',
-                maxHeight: '40vh',
-                width: '50%'
-              }}
-            />
-          </Box>
-          <Divider sx={{ my: 2 }} />
-          <Typography
-            variant="h6"
-            sx={{ color: theme.palette.secondary.dark, mb: 2 }}
-          >
-            Bình luận
-          </Typography>
-          <DocComment
-            sx={{ width: '100%', mb: 3 }}
-            docId={Number(id)}
-            documentType={DocumentType.INTERNAL}
+          <DetailAttachmentAccordion
+            attachments={data.attachments as Attachment[]}
+            watchAttachment={watchAttachment}
+            signAttachment={signAttachment}
+            addNumber={handleAddNumber}
+            sx={{ mt: 2 }}
           />
+          <DetailTimeline sx={{ mt: 2 }} processHistory={data.processHistory} />
         </Box>
       </Box>
       <AppDocViewer
