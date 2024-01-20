d6d08705894701ca89510ee2b6e24829ed197216
Long Tran <57053685+baolongt@users.noreply.github.com>
2024-01-20 13:35:53 +0700

    validate contact list form

--- src/components/dialogs/validations/create-contact-dialog.ts
+++ src/components/dialogs/validations/create-contact-dialog.ts
@@ -1,8 +1,10 @@
 import { object, string } from 'yup';
 
 export const schema = object({
-  name: string().required(`Tên phòng ban là bắt buộc`),
-  organCode: string().required(`Trưởng phòng là bắt buộc`),
-  email: string().required(`Trưởng phòng là bắt buộc`),
-  phone: string().required(`Trưởng phòng là bắt buộc`)
+  name: string().required(`Tên đơn vị là bắt buộc`),
+  organCode: string().required(`Mã đơn vị là bắt buộc`),
+  email: string()
+    .required(`Email đơn vị là bắt buộc`)
+    .email('Email không hợp lệ'),
+  phone: string().required(`Số điện thoại là bắt buộc`)
 });
